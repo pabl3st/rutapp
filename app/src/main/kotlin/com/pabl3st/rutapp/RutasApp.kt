fc152020a23205b2eec461abe7df3f23c8e5d4ed
pabl3st <pabl3st@gmail.com>
2026-05-01 05:39:02 +0200

    feat(s01): add app/src/main/kotlin/com/pabl3st/rutapp/RutasApp.kt

---
+++ app/src/main/kotlin/com/pabl3st/rutapp/RutasApp.kt
@@ -0,0 +1,21 @@
+package com.pabl3st.rutapp
+
+import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class RutasApp : Application(), Configuration.Provider {
+
+    @Inject lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .setMinimumLoggingLevel(
+                if (BuildConfig.DEBUG) android.util.Log.DEBUG else android.util.Log.ERROR
+            )
+            .build()
+}
