d9bec563bc7330657facb197c5dd157075c0097b
pabl3st <pabl3st@gmail.com>
2026-05-08 02:29:32 +0200

    fix: BibliotecaViewModel -- import kotlinx.coroutines.launch faltaba en servidor

--- app/src/main/kotlin/com/pabl3st/rutapp/feature/biblioteca/BibliotecaViewModel.kt
+++ app/src/main/kotlin/com/pabl3st/rutapp/feature/biblioteca/BibliotecaViewModel.kt
@@ -14,6 +14,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 enum class BibliotecaTab(val label: String, val icon: ImageVector) {
